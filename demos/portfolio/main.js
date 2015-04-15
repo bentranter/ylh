95ec83df629e311ffebb58c4a8fb866a4e479de8
Ben Tranter <bwtrante@lakeheadu.ca>
2015-04-14 21:45:50 -0400

    Add click to reveal functionality

--- demos/portfolio/main.js
+++ demos/portfolio/main.js
@@ -4,9 +4,17 @@
   // My latest obsession: object caching
   var button = document.getElementById('openContact');
   var form = document.getElementById('contact');
+  var showMore = document.getElementById('showMore');
+  var more = document.getElementById('more');
 
   // Listen to those damn clicky clicks
-  button.addEventListener('click', function() {
+  button.addEventListener('click', function(e) {
+    e.preventDefault();
     form.classList.toggle('hide');
+  });
+
+  showMore.addEventListener('click', function(e) {
+    e.preventDefault();
+    more.classList.toggle('hide');
   });
 })();
